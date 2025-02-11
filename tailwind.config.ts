dd968a320f10f169100335969e5207cb190cbc30
Alexey Savich <alexeysavich1112@gmail.com>
2025-02-12 00:55:14 +0300

    add sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                white: '#efeff3',
+                border: 'rgba(255, 255, 255, 0.12);'
+            }
+        }
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: []
+} satisfies Config
